2fa577c058d107bab0eb83f48112217a31917d30
felipeLx <felipealvesfinanceiro@gmail.com>
2019-11-10 19:11:35 -0300

    add cards and sound for blackjack game

--- javaScriptGame/static/js/script.js
+++ javaScriptGame/static/js/script.js
@@ -141,3 +141,26 @@ function randomColors() {
     }
 }
 
+//challenge 5: Blackjack
+let blackjackGame = {
+    'you': {'scoreSpan': '#your-blackjack-result', 
+            'div': '#your-box',
+            'score': 0},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result',
+                'div': '#dealer-box',
+                'score': 0},
+};
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+
+const hitSound = new Audio('static/sounds/swish.m4a');
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+
+function blackjackHit() {
+    let cardImage = document.createElement('img');
+    cardImage.src = 'static/images/Q.png';
+    document.querySelector(YOU['div']).appendChild(cardImage);
+    hitSound.play();
+}
